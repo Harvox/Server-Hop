6ce7aee9bcd1e258da89993ad6cce86bce22f675
Harvox <134923684+Harvox@users.noreply.github.com>
2025-01-19 05:12:12 -0500

    Create Hop.lua

---
+++ Hop.lua
@@ -0,0 +1,35 @@
+local HttpService = game:GetService("HttpService")
+local TeleportService = game:GetService("TeleportService")
+local servers = {}
+local req = request({Url = string.format("https://games.roblox.com/v1/games/%d/servers/Public?sortOrder=Desc&limit=100&excludeFullGames=true", game.PlaceId)})
+local body = HttpService:JSONDecode(req.Body)
+
+function GetServers() 
+for i, v in next, body.data do
+        if type(v) == "table" and tonumber(v.playing) and tonumber(v.maxPlayers) and v.playing < v.maxPlayers and v.id ~= game.JobId then
+        table.insert(servers, 1, v.id)
+    end
+end
+end
+
+GetServers()
+
+function Hop()
+if #servers > 0 then
+TeleportService:TeleportToPlaceInstance(game.PlaceId, servers[math.random(1, #servers)], game:GetService("Players").LocalPlayer)
+end
+end 
+
+
+if #servers <= 0 then 
+wait(5)
+GetServers()
+Hop()
+return
+end
+
+
+queue_on_teleport('print("Harvox")')
+
+
+Hop()
